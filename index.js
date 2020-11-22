9a60697b700e876ebaebc6fa2b2162e0754c1bc2
Denis Zhitnyakov <zhitnyakovda@gmail.com>
2020-11-22 13:44:41 +0300

    fixes

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const http = require('http');
 
-let intervals = [];
+const intervals = [];
 
 http.createServer((req, res) => {
   if (req.url === '/start') {
-    interval = setInterval(() => {
-      console.log(getTime())
-    }, process.env.INTERVAL_VALUE || 1000)
+    const interval = setInterval(() => {
+      console.log(getTime());
+    }, process.env.INTERVAL_VALUE || 1000);
 
     intervals.push(interval);
 
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify({success: true}));
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify({ success: true }));
     res.end();
-  } else if(req.url === '/stop') {
+  } else if (req.url === '/stop') {
     if (intervals.length > 0) clearInterval(intervals.pop());
 
-    res.setHeader("Content-Type", "application/json");
-    response = {
+    res.setHeader('Content-Type', 'application/json');
+    const response = {
       success: true,
       intervalsLeft: intervals.length,
       datetime: getTime()
-    }
-    res.write(JSON.stringify(response));
+    };
+    res.write(JSON.stringify(response));
     res.end();
   }
 }).listen(3000);
 
-function getTime() {
+function getTime () {
   return new Date(Date.now()).toUTCString();
 }
