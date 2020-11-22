921b81830e12627204670047b552ed614355a04c
Denis Zhitnyakov <zhitnyakovda@gmail.com>
2020-11-22 12:49:11 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    "no-extra-semi": "error",
+    "semi": [2, "always"]
+  }
+}
